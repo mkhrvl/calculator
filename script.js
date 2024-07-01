d290da27be358a365ed866948c83c2bec64820d5
Michael Ken Revil <60070288+mkhrvl@users.noreply.github.com>
2024-07-01 10:15:10 +0800

    feat: add function to execute math operations on two numbers

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let leftOperand = 0;
+let operator = '';
+let rightOperand = 0;
+
+function operate(leftOperand, rightOperand, operator) {
+    switch (operator) {
+        case '+':
+            return add(leftOperand, rightOperand);
+        case '-':
+            return subtract(leftOperand, rightOperand);
+        case '*':
+            return multiply(leftOperand, rightOperand);
+        case '/':
+            return divide(leftOperand, rightOperand);
+    }
+}
